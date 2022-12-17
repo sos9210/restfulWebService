978ce09da8319f66e3b7ee543c02f1d2218a96ff
sungsanghyun <sos9215@gmail.com>
2022-12-18 02:52:10 +0900

    12/17

--- src/main/java/com/example/restful/user/User.java
+++ src/main/java/com/example/restful/user/User.java
@@ -1,15 +1,28 @@
 package com.example.restful.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter @Setter
 @AllArgsConstructor
+@JsonIgnoreProperties(value = {"password"})
 public class User {
     private Integer id;
+
+    @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
+    @Past
     private Date joinDate;
+
+//    @JsonIgnore
+    private String password;
+//    @JsonIgnore
+    private String ssn;
 }
--- src/main/java/com/example/restful/user/UserController.java
+++ src/main/java/com/example/restful/user/UserController.java
@@ -4,13 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
 @RestController
 public class UserController {
     private UserDaoService service;
-
+    String asd = "a".concat("asd");
     public UserController(UserDaoService service) {
         this.service = service;
     }
@@ -22,11 +23,15 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -35,5 +40,23 @@ public class UserController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody  User user) {
+        user.setId(id);
+        User updateUser = service.update(user);
+        if(updateUser == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+        return updateUser;
     }
 }
