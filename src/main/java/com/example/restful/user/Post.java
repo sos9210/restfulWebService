d2cae62d222fb4099d9badc8df32f7bdd598b36b
sungsanghyun <sos9210@naver.com>
2022-12-22 01:18:53 +0900

    12/21

---
+++ src/main/java/com/example/restful/user/Post.java
@@ -0,0 +1,26 @@
+package com.example.restful.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
-insert into user values(1,sysdate(),'user1','test1','901010-1234567');
-insert into user values(2,sysdate(),'user2','test2','901011-1234567');
-insert into user values(3,sysdate(),'user3','test3','901312-1234567');
-insert into user values(4,sysdate(),'user4','test4','901013-1234567');
+insert into user values(9001,sysdate(),'user1','test1','901010-1234567');
+insert into user values(9002,sysdate(),'user2','test2','901011-1234567');
+insert into user values(9003,sysdate(),'user3','test3','901312-1234567');
+insert into user values(9004,sysdate(),'user4','test4','901013-1234567');
+
+insert into post values(10001,'My first post',9001);
+insert into post values(10002,'My first post',9002);
